0c60529897e74476f57b0e5a8b1ac3b73710a35a
akuan1997 <129217093+akuan1997@users.noreply.github.com>
2025-03-06 13:43:28 +0800

    輸入url/api/data可以看到前N筆資料了

---
+++ app.js
@@ -0,0 +1,52 @@
+require('dotenv').config()
+require('express-async-errors')
+
+const express = require('express')
+const app = express()
+const connectDB = require('./db/connect')
+const concertModel = require('./models/concert')
+
+const cors = require("cors");
+const port = 3000;
+
+// middleware
+app.use(cors());
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+// 路由 - 示範資料 API
+app.get("/api/data", async (req, res) => {
+    try {
+        const data = await concertModel.find().limit(6);
+        res.status(200).json(data);
+    } catch (error) {
+        res.status(500).json({error: error.message});
+    }
+});
+
+const start = async (req, res) => {
+    try {
+        // connectDB
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, () => {
+            console.log(`Server started on port ${port}`)
+        })
+    } catch (error) {
+        const start = async (req, res) => {
+            try {
+                // connectDB
+                await connectDB(process.env.MONGO_URI)
+                app.listen(port, () => {
+                    console.log(`Server started on port ${port}`)
+                })
+            } catch (error) {
+                console.log(error)
+            }
+        }
+
+        start()
+        console.log(error)
+    }
+}
+
+start()
