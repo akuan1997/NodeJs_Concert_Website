e42a62983a32a9de5b0d542d67cc209e3f667940
akuan1997 <129217093+akuan1997@users.noreply.github.com>
2025-03-06 20:26:01 +0800

    fix: 如果爬蟲沒有獲得資料，就賦值models/concert.js中設定的default

--- populate.js
+++ populate.js
@@ -3,13 +3,13 @@ require('dotenv').config();
 const connectDB = require('./db/connect');
 const Concert = require('./models/concert');
 
-const jsonProducts = require('./concert_3_5_23.json');
+const jsonConcertData = require('./concert_3_5_23.json');
 
 const start = async () => {
     try {
         await connectDB(process.env.MONGO_URI);
         await Concert.deleteMany()  // 刪除Product集合中的所有資料
-        await Concert.create(jsonProducts)  // 插入新的資料 (jsonProducts)
+        await Concert.create(jsonConcertData)  // 插入新的資料 (jsonProducts)
         console.log("DB Connected");
         process.exit(0)
     } catch (error) {
