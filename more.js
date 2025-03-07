94ca19a17e5e3b92bf5044444a297ad9c26dc086
akuan1997 <129217093+akuan1997@users.noreply.github.com>
2025-03-08 00:48:40 +0800

    feature: 日期不必選擇兩者，選擇其中一者即可顯示結果

--- more.js
+++ more.js
@@ -87,8 +87,11 @@ document.addEventListener("DOMContentLoaded", async () => {
 document.querySelector(".city_date_button").addEventListener("click", function () {
     const startDate = document.getElementById("start-date").value;
     const endDate = document.getElementById("end-date").value;
-    if (!startDate || !endDate) {
-        alert("請選擇開始和結束日期！");
+    const cityObj = document.getElementById('city').value;
+    console.log(cityObj)
+
+    if (!startDate && !endDate) {
+        alert("請選擇開始和結束日期！ 123");
         return;
     } // 跳轉到query.html頁面，並帶上日期參數
     window.location.href = `query.html?startDate=${encodeURIComponent(startDate)}&endDate=${encodeURIComponent(endDate)}`;
